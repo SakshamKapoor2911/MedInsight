aaae64c3123d9266f8c092d9ebac06bbe88b3197
Brennen Muller <mullerbrennen@gmail.com>
2025-03-29 21:09:11 -0400

    Made progress on the API changes

--- app.py
+++ app.py
@@ -25,27 +25,7 @@ class HooHacksApp:
             if latitude is None or longitude is None:
                 return "Request must contain latitude and longitude", 400
 
-            parameters = {
-                "latitude": { "$lt": latitude + 0.1 },
-                "latitude": { "$gt": latitude - 0.1 },
-                "longitude": { "$lt": longitude + 0.1 },
-                "longitude": { "$gt": longitude - 0.1 },
-            }
-
-            if specialty:
-                parameters["specialty"] = specialty
-
-            query = self.database.database.doctors.find(
-                    parameters,
-                    {"_id": 0}
-                )
-
-            doctors = []
-            for doctor in query:
-                doctor["distance"] = ((doctor["latitude"] - latitude) ** 2 + (doctor["longitude"] - longitude) ** 2) ** 0.5
-                doctors.append(doctor)
-            doctors.sort(key=lambda x: x["distance"])
-            return jsonify([doctor for doctor in doctors])
+            return jsonify(self.database.get_doctors(latitude, longitude, specialty))
 
         @self.app.route('/api/health-centers/')
         def get_health_centers():
@@ -55,36 +35,12 @@ class HooHacksApp:
             if latitude is None or longitude is None:
                 return "Request must contain latitude and longitude", 400
 
-            query = self.database.database.doctors.find(
-                    {
-                        "latitude": { "$lt": latitude + 0.1 },
-                        "latitude": { "$gt": latitude - 0.1 },
-                        "longitude": { "$lt": longitude + 0.1 },
-                        "longitude": { "$gt": longitude - 0.1 },
-                    },
-                    {
-                        "_id": 0
-                    }
-                )
-
-            doctors = []
-            for doctor in query:
-                doctor["distance"] = ((doctor["latitude"] - latitude) ** 2 + (doctor["longitude"] - longitude) ** 2) ** 0.5
-                doctors.append(doctor)
-            doctors.sort(key=lambda x: x["distance"])
-            return jsonify([doctor for doctor in doctors])
+            return jsonify(self.database.get_health_centers(latitude, longitude))
 
         @self.app.route('/<path:path>')
         def serve_static_files(path):
             file_path = os.path.join(self.app.static_folder, path)
             return send_from_directory(self.app.static_folder, path) if os.path.exists(file_path) else self.index()
-
-
-
-        # @self.app.errorhandler(404)
-        # def not_found(e):
-        #     index_path = os.path.join(self.app.static_folder, "index.html")
-        #     return send_from_directory(self.app.static_folder, "index.html") if os.path.exists(index_path) else abort(404)
 
     def run(self, host="0.0.0.0", port=5000, debug=False):
         self.app.run(host="127.0.0.1" if debug else host, port=port, debug=debug)
--- database.py
+++ database.py
@@ -21,3 +21,59 @@ class Database:
             return "Database connection successful!"
         except ConnectionFailure as e:
             return f"---ERROR--- Database connection failed: {e}"
+
+    def get_doctors(self, latitude, longitude, specialty=None):
+        parameters = {
+            "latitude": { "$lt": latitude + 0.1, "$gt": latitude - 0.1 },
+            "longitude": { "$lt": longitude + 0.1, "$gt": longitude - 0.1 },
+        }
+
+        if latitude is None or longitude is None:
+            return "Request must contain latitude and longitude", 400
+
+        query = self.database.database.doctors.find(
+            parameters,
+            {"_id": 0}
+        )
+
+        doctors = []
+        for doctor in query:
+            doctor["distance"] = ((doctor["latitude"] - latitude) ** 2 + (doctor["longitude"] - longitude) ** 2) ** 0.5
+            doctors.append(doctor)
+        doctors.sort(key=lambda x: x["distance"])
+
+        if specialty:
+            regex_conditions = [{"specialty": {"$regex": word, "$options": "i"}} for word in specialty.split()]
+            parameters["$or"] = regex_conditions
+
+        query = self.database.doctors.find(
+            parameters,
+            {"_id": 0}
+        )
+
+        doctors = []
+        for doctor in query:
+            doctor["distance"] = ((doctor["latitude"] - latitude) ** 2 + (doctor["longitude"] - longitude) ** 2) ** 0.5
+            doctors.append(doctor)
+        doctors.sort(key=lambda x: x["distance"])
+
+        return doctors
+
+    def get_health_centers(self, latitude, longitude):
+        parameters = {
+            "latitude": { "$lt": latitude + 0.1, "$gt": latitude - 0.1 },
+            "longitude": { "$lt": longitude + 0.1, "$gt": longitude - 0.1 },
+        }
+
+        query = self.database.health_centers.find(
+            parameters,
+            {"_id": 0}
+        )
+
+        health_centers = []
+        for health_center in query:
+            health_center["distance"] = ((health_center["latitude"] - latitude) ** 2 + (health_center["longitude"] - longitude) ** 2) ** 0.5
+            health_centers.append(health_center)
+        health_centers.sort(key=lambda x: x["distance"])
+
+        return health_centers
